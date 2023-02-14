93e3b42dc178648f4857480c2ed688de18550dc2
Scott Tolley <michaeltolley21@hotmail.com>
2023-02-14 20:14:21 +0100

    Adding first run notice

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo cp ./first-run-notice.txt /workspaces/.codespaces/shared/first-run-notice.txt
-chown codespace /workspaces/.codespaces/shared/first-run-notice.txt
+sudo chown codespace /workspaces/.codespaces/shared/first-run-notice.txt
