12cb174fdeb4bc12ff78999d67a942628f37f789
Scott Tolley <michaeltolley21@hotmail.com>
2023-02-18 13:26:03 +0100

    Shifting application execution to init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-# sudo cp ./first-run-notice.txt /workspaces/.codespaces/shared/first-run-notice.txt
-# sudo chown codespace /workspaces/.codespaces/shared/first-run-notice.txt
+
+JAVA_OPTS="-Xmx512m"
+ 
+# Deploy the Seeker Java Agent
+if [ ! -z "${SEEKER_SERVER_URL}" ];
+then
+    # Download the Agent package
+    curl -k -o /tmp/seeker-agent.zip "${SEEKER_SERVER_URL}/rest/api/latest/installers/agents/binaries/JAVA"
+    # Unzip the Agent package
+    unzip -d /tmp/seeker /tmp/seeker-agent.zip
+    # Append the Seeker options to the JVM options
+    JAVA_OPTS="-javaagent:/tmp/seeker/seeker-agent.jar ${JAVA_OPTS}"
+fi
+ 
+# Run the application
+java ${JAVA_OPTS} -jar api.jar
